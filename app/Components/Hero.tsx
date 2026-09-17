c6ba78f6ee758f7e496fae7bd78de4bfdc6dd1dd
darkghost87-lab <www.darkghost87@gmail.com>
2026-09-17 14:18:20 +0330

    Develop navbar and hero. ✅

--- app/Components/Hero.tsx
+++ app/Components/Hero.tsx
@@ -1,5 +1,64 @@
+import { Button } from "@/components/ui/button";
+import { ArrowBigLeft, ArrowBigRight } from "lucide-react";
+import Link from "next/link";
+
 const Hero = () => {
-    return ( <><h2>Hero Section</h2></> );
-}
- 
+  return (
+    <>
+      <section className="w-full h-[100vh] flex justify-center items-center relative z-[10]">
+        <section className="w-[50%] flex flex-col gap-10 justify-center">
+          <h1 className="w-[50%] h-[4rem] border-4 border-[#150050] px-5 flex justify-center items-center rounded text-4xl">
+            Web Developer
+          </h1>
+          <h1 className="text-6xl">Hi, I'm Amir Korouni</h1>
+          <div>
+            <h3 className="text-2xl">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse,
+              cum! Vel laboriosam nostrum laudantium dolorum molestias id
+              praesentium consectetur esse eius quae ullam deleniti non, sint in
+              cum totam quo? Lorem ipsum dolor sit amet consectetur adipisicing
+              elit. Ullam tempore nobis dolores nesciunt nihil atque consequatur
+              minus eius animi ad totam enim illo obcaecati harum recusandae
+              nulla, illum error aspernatur.
+            </h3>
+          </div>
+          <div>
+            <Link href="https://github.com">
+              <Button className="w-[20%] h-[4rem] bg-[#610094] hover:bg-[#150050] flex justify-center items-center gap-2 cursor-pointer float-right ">
+                Git hub <ArrowBigRight />
+              </Button>
+            </Link>
+          </div>
+        </section>
+        <div
+          className="
+                absolute
+                z-[-1]
+                bottom-[30%]
+                left-[12%]
+                w-[600px]
+                h-[400px]
+                rounded-full
+                bg-purple-600/15
+                blur-[120px]
+              "
+        />
+        <div
+          className="
+                absolute
+                z-[-1]
+                bottom-[10%]
+                right-[12%]
+                w-[600px]
+                h-[400px]
+                rounded-full
+                bg-blue-600/15
+                blur-[120px]
+              "
+        />
+      </section>
+    </>
+  );
+};
+
 export default Hero;
--- app/Components/NavBar.tsx
+++ app/Components/NavBar.tsx
@@ -1,7 +1,41 @@
+import Link from "next/link";
+
 const NavBar = () => {
   return (
     <>
-      <h2>NavBar</h2>
+      <nav id="navbar" className="w-full h-[5rem] flex justify-center fixed z-[11]">
+        <section className="w-[50%] h-full flex justify-between items-center mt-2 px-5 bg-[#0a0a0ae6] border rounded-[20px]">
+          <div className="flex justify-center items-center gap-2">
+            <div className="w-[40px] h-[40px] bg-[#610094] flex justify-center items-center border rounded">
+              AK
+            </div>
+            <h2>Amir Korouni</h2>
+          </div>
+          <div className="w-[50%] h-full flex justify-between items-center">
+            <ul className="size-fill flex justify-around items-center gap-5">
+              <li className="duration-200 hover:text-[#610094]">
+                <Link href="#about">About</Link>
+              </li>
+              <li className="duration-200 hover:text-[#610094]">
+                <Link href="#skills">Skills</Link>
+              </li>
+              <li className="duration-200 hover:text-[#610094]">
+                <Link href="#project">Projects</Link>
+              </li>
+              <li className="duration-200 hover:text-[#610094]">
+                <Link href="#experience">Experience</Link>
+              </li>
+              <li className="duration-200 hover:text-[#610094]">
+                <Link href="#contact">Contact</Link>
+              </li>
+            </ul>
+            <div className="flex gap-2">
+              <div>EN</div>
+              <div>Light</div>
+            </div>
+          </div>
+        </section>
+      </nav>
     </>
   );
 };

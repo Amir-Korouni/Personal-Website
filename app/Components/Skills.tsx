6c904715bb37c695810f291670599f1315e095e7
darkghost87-lab <www.darkghost87@gmail.com>
2026-09-17 16:31:06 +0330

    Develop Skills. ✅

--- app/Components/Skills.tsx
+++ app/Components/Skills.tsx
@@ -1,8 +1,16 @@
 import Container from "../style.module.css";
-import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import SkillCard from "./SkillCard";
 import { FaReact } from "react-icons/fa";
-import { SiJavascript, SiTailwindcss, SiTypescript } from "react-icons/si";
+import {
+  SiDocker,
+  SiGit,
+  SiJavascript,
+  SiLinux,
+  SiN8N,
+  SiTailwindcss,
+  SiTypescript,
+} from "react-icons/si";
+import { FaC } from "react-icons/fa6";
 
 const Skills = () => {
   return (
@@ -12,8 +20,17 @@ const Skills = () => {
           <div>
             <p className="text-zinc-100 mb-5">02 / SKILLS</p>
             <h2 className="text-6xl">My Skills</h2>
+            <p className="mt-4">
+              The tools and technologies I learn and use it.
+            </p>
           </div>
           <div className="w-full flex gap-5 justify-center items-center flex-wrap ">
+            <SkillCard
+              skill="C#"
+              desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia
+              quae, quas ipsam deserunt est voluptatum..."
+              icone={<FaC size={40} color="white" />}
+            />
             <SkillCard
               skill="JavaScript"
               desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia
@@ -38,13 +55,31 @@ const Skills = () => {
               quae, quas ipsam deserunt est voluptatum..."
               icone={<SiTailwindcss size={40} color="purple" />}
             />
+            <SkillCard
+              skill="Linux"
+              desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia
+              quae, quas ipsam deserunt est voluptatum..."
+              icone={<SiLinux size={40} />}
+            />
+            <SkillCard
+              skill="Git"
+              desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia
+              quae, quas ipsam deserunt est voluptatum..."
+              icone={<SiGit size={40} color="Orange" />}
+            />
+            <SkillCard
+              skill="Docker"
+              desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia
+              quae, quas ipsam deserunt est voluptatum..."
+              icone={<SiDocker size={40} color="blue" />}
+            />
+            <SkillCard
+              skill="N8N"
+              desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia
+              quae, quas ipsam deserunt est voluptatum..."
+              icone={<SiN8N size={40} color="red" />}
+            />
           </div>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia
-            quae, quas ipsam deserunt est voluptatum consequuntur nisi magni
-            blanditiis iusto odit a dolorem facilis et sint possimus unde, quam
-            impedit.
-          </p>
         </section>
       </section>
     </>
